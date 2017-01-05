f8660445dd9d8ddb99a30afc51377234ae314bc4
Nick Stewart <nick.stewart.programmer@gmail.com>
2017-01-05 01:56:27 -0700

    Removing github feed

--- src/Github.js
+++ src/Github.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 
 import GitHubCalendar from 'github-calendar';
 require('github-calendar/dist/github-calendar.css');
-const GitHubActivity = window.GitHubActivity;
 
 class Github extends Component {
   componentDidMount() {
     GitHubCalendar(".github-graph", "nickstew");
-    GitHubActivity.feed({ username: "nickstew", selector: "#ghfeed" });
   }
   render() {
     return (
@@ -15,7 +13,6 @@ class Github extends Component {
         <div className="section-inner">
           <h2 className="heading">My GitHub</h2>
           <div id="github-graph" className="github-graph"></div>
-          <div id="ghfeed" className="ghfeed"></div>
         </div>
       </section>
     );
